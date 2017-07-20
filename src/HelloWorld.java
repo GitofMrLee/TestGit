7dfd068cf2b9739f6731cba04ce229592a6ea354
Liming <emailforlm@163.com>
2017-07-20 11:30:33 +0800

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * 测试git的使用
+ *
+ * @author liming
+ * @create 2017-07-20 11:25
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}

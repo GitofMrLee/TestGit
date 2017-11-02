5fd95f79473a4b778b2bb3713b26c467a722c69c
Liming <emailforlm@163.com>
2017-11-02 17:24:28 +0800

    练习使用log4j

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,3 +1,5 @@
+import org.apache.log4j.Logger;
+
 /**
  * 测试git的使用
  *
@@ -6,7 +8,16 @@
  */
 
 public class HelloWorld {
+    private static Logger logger = Logger.getLogger(HelloWorld.class);
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        //System.out.println("hello world!");
+
+        logger.info("this is the info message.");
+
+        logger.debug("this is the debug message.");
+
+        logger.error("this is the error message.");
+
+
     }
 }
